1e2e64ee8e49c11e3b6715d871b6efd9d918f5c8
Danielcresp <dneli6cres@outlook.com>
2019-03-25 19:15:25 -0600

    Usar data local ,Instalar router-flux

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -8,25 +8,12 @@ export default class AlbumList extends Component<Props> {
   constructor(props) {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    
     this.state = {
-     dataSource: ds
-    }
-  }
-  componentDidMount(){
-    this.updateDataSource(this.props.albums)
-  }
-
-  componentWillReceiveProps(newProps){ //cada ves que cambia las probiedades se ejecuta 
-    if(newProps.albums !== this.props.albums){
-    this.updateDataSource(newProps.albums)
-    }
+      dataSource: ds.cloneWithRows(props.albums)
+    };
   }
   
-  updateDataSource =data =>{
-    this.setState = ({
-      dataSource: this.dataSource.cloneWithRows(data)
-    })
-  }
 
   render() {
     return (
--- src/HomeView.js
+++ src/HomeView.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet,View} from 'react-native';
-import AlbumList from './src/AlbumList';
+import AlbumList from './AlbumList';
 require('console');
 
 type Props = {};
